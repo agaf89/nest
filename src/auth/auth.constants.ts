082172b24178e6e11826d5d65e63cd7505080cad
Agafonov.Stas <agafon_stas89@mail.ru>
2022-01-07 17:19:00 +0300

    ABC-3 added JWT auth

---
+++ src/auth/auth.constants.ts
@@ -0,0 +1,3 @@
+export const ALREADY_REGISTERED_ERROR = 'Такой пользователь существует';
+export const USER_NOT_FOUND = 'Пользователь с таким email не найден';
+export const WRONG_PASSWORD = 'Неверный пароль';
